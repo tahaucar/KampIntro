d8963e0497c6798c72a7c9550ec596a9a0f05d2c
ztaha1996@gmail.com <ztaha1996@gmail.com>
2021-01-31 18:47:01 +0300

    MyList için

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler= new MyList<string>();
+            isimler.add("ali");
+            isimler.add("ayşe");
+            isimler.add("ahmet");
+            isimler.add("osman");
+            isimler.add("fatma");
+            Console.WriteLine(isimler.Count);
+        }
+    }
+}
